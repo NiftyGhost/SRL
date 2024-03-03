3b02c82ee7fc80fcb6ec2175f8045722d6c1e7d6
Blob <161654324+BlobbyBoi04@users.noreply.github.com>
2024-03-02 22:03:35 -0500

    Create PointerArrays.c

---
+++ C Files/PointerArrays.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[] = {11,13,17,19};
+    int *ptr;
+
+    //Location of the entire array (and it's first element)
+    ptr = array;
+
+    //Prints the first element of the array
+    printf("The element is %d\n", *ptr);
+
+    for(int x = 0; x < 4; x++)
+    {
+        //Prints the value of the current element of the array
+        printf("Element %d is %d\n", x + 1, *ptr);
+        //Prints the address of the current element of the array
+        printf("It's in address %p\n", ptr);
+
+        //Goes to the next address of the array
+        ptr++;
+    }
+
+    return 0;
+}
